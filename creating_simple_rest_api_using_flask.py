34c2e3bc3edcb1aff96ddcf6f6d438a0d6fb6554
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-26 12:38:02 +0800

    pseudo codee

---
+++ creating_simple_rest_api_using_flask.py
@@ -0,0 +1,17 @@
+# ========== IMPORT/S =========
+
+# ========== PSEUDO CODE =========
+# - Json Format
+
+# - Base URL
+
+# - App.route of get all 
+
+# - App.route of service id
+
+# - App.route of add service
+
+# - App.route of delete service
+
+# - App.route of update service
+
